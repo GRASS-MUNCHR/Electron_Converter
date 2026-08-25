1b2e09afc5e8c7d3da27f618f0d02c8fa2a285da
GRASS-MUNCHR <mijauntalbot@gmail.com>
2026-08-25 00:51:09 -0500

    Added light/dark modes and toggle, as well as an option to sync to the system preference.
    Not sure what's going on with the glass background; check later.

--- my-electron-app/renderer.js
+++ my-electron-app/renderer.js
@@ -1,10 +1,9 @@
-const information = document.getElementById("info");
+document.getElementById("light-dark-toggle").addEventListener("click", async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById("theme-source").innerHTML = isDarkMode ? "Dark" : "Light"
+})
 
-const func = async () => {
-    const response = await window.versions.ping()
-    console.log(response)
-}
-
-func()
-
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+document.getElementById("sync-btn").addEventListener("click", async () => {
+    await window.darkMode.system()
+    document.getElementById("theme-source").innerHTML = "System"
+})
